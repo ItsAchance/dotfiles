85749400267a0d0a03b1457a2d6b7e52617b7380
ItsAchance <alexander.chance@tutanota.com>
2024-07-24 22:43:39 +0200

    Redone the entire dotfiles structure to better and easier stow specific files

---
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,24 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
