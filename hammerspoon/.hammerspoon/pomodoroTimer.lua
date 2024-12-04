9b4275670383bad4bbeb689c46819061b8071cb3
ItsAchance <alexander.chance@tutanota.com>
2024-12-04 22:35:47 +0100

    Finished pomodoro script for now

--- hammerspoon/.hammerspoon/pomodoroTimer.lua
+++ hammerspoon/.hammerspoon/pomodoroTimer.lua
@@ -1,5 +1,23 @@
-local phrase = "Time for a break"
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "P", function()
-	hs.notify.new({ title = "Hammerspoon Pomodoro", informativeText = phrase }):send()
-end)
+local timerStart = "Starting Pomodoro timer"
+local timerEnd = "Time's up, time for a break"
+local timerOn = false
 
+
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "P", function()
+	if timerOn == false then
+		local notification = hs.notify.new({ title = "Pomodoro", informativeText = timerStart })
+		notification:alwaysPresent()
+		notification:send()
+		timerOn = true
+		for time = 1, 10 do
+            local sleep = os.execute('sleep 1')
+			if time == 10 then
+				local notification =
+					hs.notify.new({ title = "Pomodoro", informativeText = timerEnd, withdrawAfter = 0 })
+				notification:alwaysPresent()
+				notification:send()
+                timerOn = false
+			end
+		end
+	end
+end)
