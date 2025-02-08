bd86ed71b892787a60901d0704d41cdeab130ee2
ItsAchance <alexander.chance@tutanota.com>
2025-02-08 07:39:42 +0100

    Some ricing and changing to ghostty instead of kitty

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"williamboman/mason.nvim",
-		lazy = false,
+	lazy = false,
 		config = function()
 			require("mason").setup()
 		end,
@@ -54,7 +54,6 @@ return {
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.code_action, {})
 			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
 		end,
 	},
