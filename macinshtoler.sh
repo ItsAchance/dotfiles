7ca895a94866fac850bf3cd0924877370f0a45b3
ItsAchance <alexander.chance@tutanota.com>
2025-05-26 13:44:23 +0200

    First lines of my macinshtoler script

---
+++ macinshtoler.sh
@@ -0,0 +1,6 @@
+# Sets wallpaper
+osascript -e osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/alexander.chance/backgrounds/lofiwallpaper.png"'
+
+# Sets dock to autohide
+defaults write com.apple.dock autohide -int 0
+
