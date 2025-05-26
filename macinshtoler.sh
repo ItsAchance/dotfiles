0b9383dd082fa71cc6b87c8cf4ace96595ce7173
ItsAchance <alexander.chance@tutanota.com>
2025-05-26 13:47:28 +0200

    Added shebang

--- macinshtoler.sh
+++ macinshtoler.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Sets wallpaper
 osascript -e osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/alexander.chance/backgrounds/lofiwallpaper.png"'
 
