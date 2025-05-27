6c1fefb26b3299498e4417a3ff836a36c88cfcd5
ItsAchance <alexander.chance@tutanota.com>
2025-05-27 09:36:26 +0200

    Added config for hot corners and put them all to no-op

--- macinshtoler.sh
+++ macinshtoler.sh
@@ -15,3 +15,34 @@ defaults write com.apple.dock tilesize -int 70
 # Sets Caps Lock to left Control
 hidutil property --set '{"UserKeyMapping": [ {"HIDKeyboardModifierMappingSrc":0x700000039, "HIDKeyboardModifierMappingDst":0x7000000E0} ]}'
 
+# Hot corners
+# Possible values:
+#  0: no-op
+#  2: Mission Control
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+
+# Bottom right screen corner
+defaults write com.apple.dock wvous-br-corner -int 0
+defaults write com.apple.dock wvous-br-modifier -int 0
+
+# Top right screen corner
+defaults write com.apple.dock wvous-tr-corner -int 0
+defaults write com.apple.dock wvous-tr-modifier -int 0
+
+# Bottom left screen corner
+defaults write com.apple.dock wvous-bl-corner -int 0
+defaults write com.apple.dock wvous-bl-modifier -int 0
+
+# Top left screen corner
+defaults write com.apple.dock wvous-tl-corner -int 0
+defaults write com.apple.dock wvous-tl-modifier -int 0
+
+
+
