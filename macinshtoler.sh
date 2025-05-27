39a78f54e251dae415c80f1a6b0bc626b6867449
ItsAchance <alexander.chance@tutanota.com>
2025-05-27 09:28:36 +0200

    More dock settings and remap Caps lock to left Control

--- macinshtoler.sh
+++ macinshtoler.sh
@@ -3,6 +3,15 @@
 # Sets wallpaper
 osascript -e osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/alexander.chance/backgrounds/lofiwallpaper.png"'
 
+# Sets the dock to the left
+defaults write com.apple.dock orientation left
+
 # Sets dock to autohide
 defaults write com.apple.dock autohide -int 0
+
+# Sets dock icon size
+defaults write com.apple.dock tilesize -int 70
+
+# Sets Caps Lock to left Control
+hidutil property --set '{"UserKeyMapping": [ {"HIDKeyboardModifierMappingSrc":0x700000039, "HIDKeyboardModifierMappingDst":0x7000000E0} ]}'
 
