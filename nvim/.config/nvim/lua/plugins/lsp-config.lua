29fd8148ae82cd23973f5d826fec9f4b3700542b
ItsAchance <contact@alexanderchance.me>
2026-01-13 21:31:27 +0100

    Sorted ensure installed lsp's and removed nvim-lspconfig config

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -15,23 +15,23 @@ return {
         config = function()
             require("mason-lspconfig").setup({
                 ensure_installed = {
-                    "lua_ls",
-                    "ts_ls",
-                    "html",
                     "cssls",
                     "gopls",
-                    "templ",
+                    "html",
+                    "lua_ls",
                     "tailwindcss",
+                    "templ",
+                    "ts_ls",
                 },
                 automatic_enable = {
                     exclude = {
-                        "lua_ls",
-                        "ts_ls",
-                        "html",
                         "cssls",
                         "gopls",
-                        "templ",
+                        "html",
+                        "lua_ls",
                         "tailwindcss",
+                        "templ",
+                        "ts_ls",
                     },
                 },
             })
@@ -39,38 +39,6 @@ return {
     },
     {
         "neovim/nvim-lspconfig",
-        lazy = false,
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-            local lspconfig = require("lspconfig")
-            lspconfig.gopls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.pyright.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.html.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.ts_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.cssls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.templ.setup({
-                capabilities = capabilities,
-            })
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-            vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
-        end,
     },
     -- Display LSP diagnostics
     vim.diagnostic.config({ virtual_text = true }),
