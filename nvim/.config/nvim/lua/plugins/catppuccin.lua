a698da4091bc87f0dc6a64a5708b54a02349b44c
ItsAchance <contact@alexanderchance.me>
2026-04-28 21:19:44 +0200

    fix(neovim): Fixes for neovim 0.12

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -7,6 +7,12 @@ return {
         config = function()
             require("catppuccin").setup({
                 show_end_of_buffer = true,
+                custom_highlights = function(colors)
+                    return {
+                        NormalFloat = { bg = colors.surface0 },
+                        FloatBorder = { fg = colors.blue, bg = colors.surface0 },
+                    }
+                end,
             })
             vim.cmd.colorscheme "catppuccin-mocha"
         end
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,29 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  }
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "main",
+        lazy = false,
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter").install({
+                "lua", "vim", "vimdoc", "query",
+                "markdown", "markdown_inline",
+                "bash", "json", "yaml", "toml",
+                "go", "gomod", "gosum",
+                "html", "css", "javascript", "typescript", "tsx",
+            })
+
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function(args)
+                    local ft = args.match
+                    local lang = vim.treesitter.language.get_lang(ft) or ft
+                    if pcall(vim.treesitter.language.add, lang) then
+                        pcall(vim.treesitter.start, args.buf, lang)
+                        vim.bo[args.buf].indentexpr =
+                        "v:lua.require'nvim-treesitter'.indentexpr()"
+                    end
+                end,
+            })
+        end,
+    },
 }
